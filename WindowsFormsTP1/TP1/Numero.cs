041957bec03608d45cb1eb4ea4a4399a861e2df7
AxelBritok <axeel.emmanuel@outlook.com>
2017-09-14 17:04:40 -0300

    Add files via upload

---
+++ WindowsFormsTP1/TP1/Numero.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP1
+{
+    public class Numero
+    {
+        /// <summary>
+        /// atributo privado de tipo double llamado numero
+        /// </summary>
+        private double numero;
+        
+        /// <summary>
+        /// constructor por defecto, inicializa numero en 0
+        /// </summary>
+        public Numero()
+        {
+            this.numero = 0;
+        }
+
+        /// <summary>
+        /// Sobrecargar de constructor con un string ingresando por parametro para inicializar numero
+        /// </summary>
+        /// <param name="numero"> numero contiene un string que puede ser un numero </param>
+        public Numero(string numero)
+        {
+            setNumero(numero);
+        }
+
+        /// <summary>
+        /// Sobrecargar de constructor con un double ingresando por parametro para inicializar numero
+        /// </summary>
+        /// <param name="numero">numero contiene un double para inicializar el atributo numero</param>
+        public Numero(double numero)
+        {
+            this.numero = numero;
+        }
+
+        /// <summary>
+        /// getNumero tiene la funcion de actuar como un getter para retornar el atributo numero
+        /// </summary>
+        /// <returns> retorna el atributo numero</returns>
+        public double getNumero()
+        {
+            return this.numero;
+        }
+
+        /// <summary>
+        /// funcion encargada de validar un numero con el TryParse
+        /// </summary>
+        /// <param name="numeroString"> numero contiene un string que sera validado con el fin de que sea un numero </param>
+        /// <returns> retorna el resultado de la validacion </returns>
+        private double validarNumero(string numeroString)
+        {
+            double retorno;
+
+            double.TryParse(numeroString, out retorno);
+
+            return retorno;
+        }
+
+        /// <summary>
+        /// la funcion se encarga de settear el atributo numero
+        /// </summary>
+        /// <param name="numero"> numero contiene un string que puede ser un numero </param>
+        private void setNumero(string numero)
+        {
+            this.numero = validarNumero(numero);
+        }
+        
+    }
+}
---
+++ WindowsFormsTP1/WindowsFormsTP1/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TP1;
+
+namespace WindowsFormsTP1
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Funcion que inicia el WindowsForm y carga los datos iniciales en los textBox, ComboBox, y titulos.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.Name = "Calculadora";
+            this.Text = "Calculadora";
+
+            lblResultado.Text = "";
+
+            cmbOperacion.Items.Add("+");
+            cmbOperacion.Items.Add("-");
+            cmbOperacion.Items.Add("*");
+            cmbOperacion.Items.Add("/");
+
+            btnLimpiar.Text = "CC";
+            btnOperar.Text = "=";
+        }
+
+        /// <summary>
+        /// Cuando el usuario clickea el button btnOperar llama a esta funcion, que a su vez esta llama a operar para realizar la operacion.
+        /// </summary>
+        /// <param name="sender">(todavia no aprendido)</param>
+        /// <param name="e">(todavia no aprendido)</param>
+        private void btnOperar_Click(object sender, EventArgs e) { operar(); }
+
+        /// <summary>
+        /// Cuando el usuario clickea el button btnLimpiar llama a esta funcion, que a su vez esta llama a limpiar para realizar la operacion.
+        /// </summary>
+        /// <param name="sender">(todavia no aprendido)</param>
+        /// <param name="e">(todavia no aprendido)</param>
+        private void btnLimpiar_Click(object sender, EventArgs e) { limpiar(); }
+
+        /// <summary>
+        /// funcion que realiza la operacion de 2 numeros dependiendo del operador extraido del comboBox, luego carga el resultado en el label.
+        /// </summary>
+        private void operar()
+        {
+            Numero numero1 = new Numero(txtNumero1.Text);
+            Numero numero2 = new Numero(txtNumero2.Text);
+
+            lblResultado.Text = Calculadora.operar(numero1, numero2, Calculadora.validarOperador(cmbOperacion.SelectedItem.ToString())).ToString();
+        }
+
+        /// <summary>
+        /// funcion que realiza una limpieza de los casilleros del label y los textBoxs.
+        /// </summary>
+        private void limpiar()
+        {
+            lblResultado.Text = "";
+
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+        }   
+        
+    }
+}
