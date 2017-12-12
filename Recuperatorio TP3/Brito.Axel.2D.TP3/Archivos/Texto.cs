ab761091c2dd067057bb09da523e8ca947f18999
AxelBritok <axeel.emmanuel@outlook.com>
2017-12-12 02:26:43 -0300

    Recuperatorio TP3

---
+++ Recuperatorio TP3/Brito.Axel.2D.TP3/Archivos/Texto.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+
+        /// <summary>
+        /// Escribe la secuencia. Guarda el archivo.
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns name= "aux" ></returns>
+
+        public bool guardar(string archivo, string datos)
+        {
+            bool aux = true;
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(datos);
+                   
+                }
+            }
+            catch
+            {
+                aux = false;
+                throw new Exception("No se pudo guardar en el archivo");
+            }
+            return aux;
+        }
+
+        /// <summary>
+        /// Lectura de secuencia.
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns name= "aux" ></returns>
+        public bool leer(string archivo, out string datos)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            bool aux = true;
+            try
+            {
+                using (StreamReader reader = new StreamReader(archivo))
+                    sb.Append(reader.ReadToEnd());
+               
+            }
+            catch
+            {
+                aux = false;
+                throw new Exception("No se pudo leer del archivo");
+
+            }
+            datos = sb.ToString();
+            return aux;
+        }
+    }
+}
---
+++ Recuperatorio TP3/Brito.Axel.2D.TP3/Archivos/Xml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Archivos.cs
+{
+    public class Xml<T> : IArchivo<T> where T : class
+    {
+        /// <summary>
+        /// Serealiza y escribe el documento xml.
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns name= "aux" ></returns>
+        /// 
+        public bool guardar(string archivo, T datos)
+        {
+            bool aux = true;
+            try
+            {
+                using (XmlTextWriter writer = new XmlTextWriter(archivo, Encoding.UTF8))
+                {
+                    XmlSerializer serializador = new XmlSerializer(typeof(T));
+
+                    serializador.Serialize(writer, datos);
+                }
+            }
+            catch
+            {
+                aux = false;
+                throw new Exception("No se pudo guardar el archivo como Xml.");
+            }
+            return aux;
+        }
+
+        /// <summary>
+        /// Deserealiza Xml
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns name= "aux" ></returns>
+
+        public bool leer(string archivo, out T datos)
+        {
+            bool aux = true;
+            try
+            {
+                using (XmlTextReader lector = new XmlTextReader(archivo))
+                {
+                    XmlSerializer serializador = new XmlSerializer(typeof(T));
+
+                   
+                    datos = (T)serializador.Deserialize(lector);
+                }
+            }
+            catch
+            {
+                aux = false;
+                datos = null;
+                throw new Exception("No se pudo leer del archivo.");
+            }
+            return aux;
+        }
+    }
+}
