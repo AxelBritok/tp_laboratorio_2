5d4c1063a0abf856de447655aca2a4de6f94a6c1
AxelBritok <axeel.emmanuel@outlook.com>
2017-12-12 02:17:52 -0300

    Add files via upload

---
+++ Recuperatorio TP4/Brito.Axel.2D.TP4/Navegador/frmHistorial.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Navegador
+{
+    partial class frmHistorial
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstHistorial = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lstHistorial
+            // 
+            this.lstHistorial.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lstHistorial.FormattingEnabled = true;
+            this.lstHistorial.Location = new System.Drawing.Point(0, 0);
+            this.lstHistorial.Name = "lstHistorial";
+            this.lstHistorial.Size = new System.Drawing.Size(384, 361);
+            this.lstHistorial.TabIndex = 0;
+            // 
+            // frmHistorial
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 361);
+            this.Controls.Add(this.lstHistorial);
+            this.Name = "frmHistorial";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Historial";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstHistorial;
+    }
+}
+
